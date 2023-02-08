916433ebbf1c976b8c6666155b18dee4f4d3b584
Kevin Chacha <0kevcha0@gmail.com>
2023-02-08 14:40:34 -0500

    Styling improvements made

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,23 +1,19 @@
 // Import the functions you need from the SDKs you need
 
-import { initializeApp } from "firebase/app";
+import { getApp, getApps, initializeApp } from "firebase/app";
 
-import { getAnalytics } from "firebase/analytics";
-
-import {getAuth} from "firebase/auth";
-import {getFirestore} from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 
 // Your web app's Firebase configuration
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
 
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
@@ -29,15 +25,10 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
 
   appId: process.env.NEXT_PUBLIC_APP_ID,
-
-  measurementId: process.env.NEXT_PUBLIC_MEASURE_ID
-
 };
-
 
 // Initialize Firebase
 
-const app = initializeApp(firebaseConfig);
-
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 export const auth = getAuth();
 export const db = getFirestore(app);
